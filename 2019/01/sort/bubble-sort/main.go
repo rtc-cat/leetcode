2697b2fe1b44f5d691bbd2c752385c3f06f5d213
caobo <caobo@didapinche.com>
2019-01-09 22:27:01 +0800

    修改冒泡排序实现

--- 2019/01/sort/bubble-sort/main.go
+++ 2019/01/sort/bubble-sort/main.go
@@ -24,14 +24,11 @@ func main() {
 
 // 冒泡排序
 func bubbleSort(arr []int) {
-	for count := 0; count < len(arr); count++ {
-		i, j := 0, 1
-		for j < len(arr) {
-			if arr[i] > arr[j] {
-				arr[i], arr[j] = arr[j], arr[i]
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
-			i++
-			j++
 		}
 	}
 }
