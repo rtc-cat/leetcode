91297de6a725f672a70765fd557ab646984c00ec
caobo <caobo@didapinche.com>
2019-04-16 15:53:14 +0800

    完成平衡二叉树

--- 2019/01/balanced-binary-tree/main.go
+++ 2019/01/balanced-binary-tree/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"math"
+
+	. "github.com/krizss/leetcode/utils/treeutil"
+)
+
 // 给定一个二叉树，判断它是否是高度平衡的二叉树。
 
 // 本题中，一棵高度平衡二叉树定义为：
@@ -30,14 +36,6 @@ package main
 //  4   4
 // 返回 false 。
 func main() {
-
-}
-
-// TreeNode 树节点
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
 }
 
 /**
@@ -49,31 +47,30 @@ type TreeNode struct {
  * }
  */
 func isBalanced(root *TreeNode) bool {
-	// 递归退出条件 如果两边的节点的子节点都没有节点
-	if root == nil {
-		return true
-	}
-	// 如果两个子节点都是空
-	if root.Left == nil && root.Right == nil {
-		return true
-	}
-	// 如果两个都不为空
-	if root.Left != nil && root.Right != nil {
-		return isBalanced(root.Left) && isBalanced(root.Right)
-	}
-	// 这里就是一个为空,另一个不为空的情况
-	left := check(root.Left)
-	right := check(root.Right)
-	return left == right
+	b, _ := check(root)
+	return b
 }
 
 // 判断节点是否有子节点
-func check(node *TreeNode) bool {
-	if node != nil {
-		if node.Left == nil && node.Right == nil {
-			return false
-		}
-		return true
+func check(node *TreeNode) (bool, int) {
+	// 退出条件
+	if node == nil {
+		return true, 0
 	}
-	return false
+	// 检查两个节点是否平衡,以及深度
+	lb, ld := check(node.Left)
+	rb, rd := check(node.Right)
+	// 如果这俩有一个不平衡,那么就返回false
+	if !lb || !rb {
+		return false, 0
+	}
+	// 检查这俩树的深度,如果差距不是1,就返回false
+	if math.Abs(float64(ld-rd)) > 1 {
+		return false, 0
+	}
+	max := ld
+	if rd > ld {
+		max = rd
+	}
+	return true, max + 1
 }
