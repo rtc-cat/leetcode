5e388202be734e2b76b44be156210254f7151479
krizss <cb.kris.bj@gmail.com>
2019-12-06 11:48:29 +0800

    最长有效括号

--- stack/longest-valid-parentheses_test.go
+++ stack/longest-valid-parentheses_test.go
@@ -19,10 +19,54 @@ import "testing"
 // 链接：https://leetcode-cn.com/problems/longest-valid-parentheses
 
 func TestLongestValidParentheses(t *testing.T) {
-
+	testCases := []struct{ input string }{
+		{"())"},
+		{"()"},
+		{")()())"},
+		{"(()())"},
+		{"(()"},
+		{")()())"},
+		{"()(())"},
+	}
+	for _, tc := range testCases {
+		t.Log(longestValidParentheses(tc.input))
+	}
 }
 
 func longestValidParentheses(s string) int {
-
-	return 0
+	if len(s) < 2 {
+		return 0
+	}
+	var max int
+	// 每一个数字表示当前位置之前的最优结果
+	dp := make([]int, len(s))
+	// 状态转移方程
+	for i := 1; i < len(s); i++ {
+		if s[i] == ')' {
+			if s[i-1] == '(' {
+				if i >= 2 {
+					dp[i] = dp[i-2] + 2
+				} else {
+					dp[i] = 2
+				}
+			} else if s[i-1] == ')' {
+				// 计算上一个括号的index
+				lastLeft := i - dp[i-1] - 1
+				if lastLeft >= 0 {
+					if s[lastLeft] == '(' {
+						if lastLeft >= 1 {
+							// 那就应该是这个index左边的值加上现在的值
+							dp[i] = dp[i-1] + dp[lastLeft-1] + 2
+						} else {
+							dp[i] = dp[i-1] + 2
+						}
+					}
+				}
+			}
+		}
+		if dp[i] > max {
+			max = dp[i]
+		}
+	}
+	return max
 }
