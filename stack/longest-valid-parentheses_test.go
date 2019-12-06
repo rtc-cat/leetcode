8f7d5ac81abc065f9ff7f82de5497af4403609f6
krizss <cb.kris.bj@gmail.com>
2019-12-06 10:51:23 +0800

    小岛的数量

---
+++ stack/longest-valid-parentheses_test.go
@@ -0,0 +1,28 @@
+package stack
+
+import "testing"
+
+// 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+
+// 示例 1:
+
+// 输入: "(()"
+// 输出: 2
+// 解释: 最长有效括号子串为 "()"
+// 示例 2:
+
+// 输入: ")()())"
+// 输出: 4
+// 解释: 最长有效括号子串为 "()()"
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/longest-valid-parentheses
+
+func TestLongestValidParentheses(t *testing.T) {
+
+}
+
+func longestValidParentheses(s string) int {
+
+	return 0
+}
