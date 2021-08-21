9a6a9d5a87e2b53f7e6d0b99de87b7ab7cd880d6
krizsx <cb.kris.bj@gmail.com>
2021-08-22 00:58:06 +0800

    :construction_worker: add demo

---
+++ README.md
@@ -0,0 +1,3 @@
+# leetcode记录
+
+[package和create](https://kaisery.github.io/trpl-zh-cn/ch07-01-packages-and-crates.html)
---
+++ src/solution/s443.rs
@@ -0,0 +1,31 @@
+#[allow(dead_code)]
+struct Solution {}
+
+impl Solution {
+    #[allow(dead_code)]
+    fn compress(chars: &mut Vec<char>) -> i32 {
+        if chars.len() == 1 {
+            return 1;
+        }
+        0
+    }
+}
+
+#[test]
+fn test_solution() {
+    let test_cases = vec![
+        ("1个字符", vec!['a'], 1),
+        ("通过", vec!['a', 'a', 'b', 'b', 'c', 'c', 'c'], 0),
+        (
+            "多个字符",
+            vec![
+                'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b',
+            ],
+            0,
+        ),
+    ];
+    for (name, mut chars, result) in test_cases {
+        println!("{}测试:", name);
+        assert_eq!(Solution::compress(&mut chars), result);
+    }
+}
