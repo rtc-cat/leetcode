57c02991a279395c96211c6471d1f72ec69caf34
krizsx <cb.kris.bj@gmail.com>
2021-08-29 00:25:30 +0800

    :white_check_mark: add 295

--- src/solution/s295.rs
+++ src/solution/s295.rs
@@ -1,6 +1,12 @@
-#[allow(dead_code)]
-struct MedianFinder {}
+use std::collections::BinaryHeap; // 默认是最大堆
 
+// 官方库 最大堆,最小堆 https://doc.rust-lang.org/std/collections/struct.BinaryHeap.html
+#[allow(dead_code)]
+struct MedianFinder {
+    cnt: usize,
+    min_hp: BinaryHeap<i32>,
+    max_hp: BinaryHeap<i32>,
+}
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
@@ -9,15 +15,33 @@ impl MedianFinder {
     /** initialize your data structure here. */
     #[allow(dead_code)]
     fn new() -> Self {
-        MedianFinder {}
+        MedianFinder {
+            cnt: 0,
+            max_hp: BinaryHeap::new(),
+            min_hp: BinaryHeap::new(),
+        }
     }
 
     #[allow(dead_code)]
-    fn add_num(&self, num: i32) {}
+    fn add_num(&mut self, num: i32) {
+        self.cnt += 1;
+        self.max_hp.push(-num);
+        self.min_hp.push(-self.max_hp.pop().unwrap());
+        if self.cnt & 1 == 1 {
+            self.max_hp.push(-self.min_hp.pop().unwrap());
+        }
+    }
 
     #[allow(dead_code)]
     fn find_median(&self) -> f64 {
-        0.
+        if self.cnt == 0 {
+            return 0.0;
+        }
+        if self.cnt & 1 == 1 {
+            -self.max_hp.peek().unwrap() as f64
+        } else {
+            (self.min_hp.peek().unwrap() - self.max_hp.peek().unwrap()) as f64 / 2.0
+        }
     }
 }
 
@@ -26,5 +50,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_solution() {}
+    fn test_solution() {
+        let mut obj = MedianFinder::new();
+        assert_eq!(obj.find_median(), 0.0);
+
+        obj.add_num(1);
+        assert_eq!(obj.find_median(), 1.0);
+
+        obj.add_num(2);
+        assert_eq!(obj.find_median(), 1.5);
+
+        obj.add_num(3);
+        assert_eq!(obj.find_median(), 2.0);
+    }
 }
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env zsh
-cargo test -- --color always --nocapture
+PROBLEM=$1
+echo "运行第${PROBLEM}题测试用例"
+cargo test s${PROBLEM} -- --color always --nocapture
